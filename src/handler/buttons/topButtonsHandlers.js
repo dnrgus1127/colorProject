c681931942293495405f44d34464bb93f2a0433c
dnrgus1127 <dnrgus1127@naver.com>
2023-10-04 22:18:16 +0900

    Refactoring (view) : 캡슐화
    팔레트 색상을 업데이트하는 함수를 ColorPaletteList 객체의 메서드로 변경

--- src/handler/buttons/topButtonsHandlers.js
+++ src/handler/buttons/topButtonsHandlers.js
@@ -1,23 +1,27 @@
 import { Color } from "../../constructor/Color.js";
-import { updatePaletteColor } from "../../view/updatePaletteView.js";
+import { colorPaletteList } from "../../script.js";
 
-export function topButtonsHandlers(currentPalette) {
+export function topButtonsHandlers() {
 
     const $complementaryButton = document.getElementById("complementaryButton");
 
     $complementaryButton.addEventListener("click", () => {
+        const currentPalette = colorPaletteList.getCurrentPalette();
         const complementaryColor = currentPalette.getMainColor().getComplementary();
+
         currentPalette.setMainColor(complementaryColor);
-        updatePaletteColor();
+        colorPaletteList.rePaintPalette();
 
     })
 
     const $randomColorButton = document.getElementById("randomColorButton");
 
     $randomColorButton.addEventListener("click", () => {
+        const currentPalette = colorPaletteList.getCurrentPalette();
+
         currentPalette.setMainColor(Color.randomColor());
         currentPalette.setBaseColor(Color.randomColor());
-        updatePaletteColor();
+        colorPaletteList.rePaintPalette();
     })
 
 }
