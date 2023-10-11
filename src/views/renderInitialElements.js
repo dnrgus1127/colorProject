12af39bf2e0761adc4cbf21deb65914e1b9bc08b
dnrgus1127 <dnrgus1127@naver.com>
2023-10-11 15:53:48 +0900

    DESIGN : CSS 중복 제거

--- src/views/renderInitialElements.js
+++ src/views/renderInitialElements.js
@@ -7,7 +7,6 @@ export function renderInitialElements() {
 
     renderColoPalette();
     renderNewPaletteContainer();
-    //renderContrastContainer();
     renderAppComponent();
 }
 
@@ -18,22 +17,21 @@ function renderColoPalette() {
     $colorPalette.innerHTML = "";
     //
 
-    const colorItemList = document.createElement("div");
-    colorItemList.className = "colorItemList";
+    const colorItemList = customCreateElement("div.colorItemList");
+
 
     $colorPalette.appendChild(colorItemList);
 
     (new Array(12).fill(0)).forEach(() => {
-        const colorItemBox = document.createElement("div");
-        colorItemBox.className = "colorItemBox";
+        const colorItemBox = customCreateElement("div.colorItemBox.flex-center");
 
-        const colorCode = document.createElement("p");
-        colorCode.className = "colorCode";
 
-        const colorItemButtonBox = document.createElement("div");
-        colorItemButtonBox.className = "colorItemButtonBox";
-        const exChangeButton = document.createElement("button");
-        exChangeButton.className = "exChangeButton";
+        const colorCode = customCreateElement("p.colorCode");
+
+
+        const colorItemButtonBox = customCreateElement("div.colorItemButtonBox");
+
+        const exChangeButton = customCreateElement("button.exChangeButton.button-svg")
 
         // 교체 아이콘
         exChangeButton.innerHTML = `<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2"
@@ -42,8 +40,7 @@ function renderColoPalette() {
             d="m2.179 10.201c.055-.298.393-.734.934-.59.377.102.612.476.543.86-.077.529-.141.853-.141 1.529 0 4.47 3.601 8.495 8.502 8.495 2.173 0 4.241-.84 5.792-2.284l-1.251-.341c-.399-.107-.636-.519-.53-.919.108-.4.52-.637.919-.53l3.225.864c.399.108.637.519.53.919l-.875 3.241c-.107.399-.519.636-.919.53-.399-.107-.638-.518-.53-.918l.477-1.77c-1.829 1.711-4.27 2.708-6.838 2.708-5.849 0-9.968-4.8-10.002-9.93-.003-.473.027-1.119.164-1.864zm19.672 3.6c-.054.298-.392.734-.933.59-.378-.102-.614-.476-.543-.86.068-.48.139-.848.139-1.53 0-4.479-3.609-8.495-8.5-8.495-2.173 0-4.241.841-5.794 2.285l1.251.341c.4.107.638.518.531.918-.108.4-.519.637-.919.53l-3.225-.864c-.4-.107-.637-.518-.53-.918l.875-3.241c.107-.4.518-.638.918-.531.4.108.638.518.531.919l-.478 1.769c1.83-1.711 4.272-2.708 6.839-2.708 5.865 0 10.002 4.83 10.002 9.995 0 .724-.081 1.356-.164 1.8z"
             fill-rule="nonzero" />
         </svg>`
-        const clipboardButton = document.createElement("button");
-        clipboardButton.className = "clipboardButton";
+        const clipboardButton = customCreateElement("button.clipboardButton.button-svg")
         clipboardButton.innerHTML = `<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round"
         stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path
@@ -51,8 +48,8 @@ function renderColoPalette() {
             fill-rule="nonzero" />
         </svg>`
 
-        const overlay = document.createElement("div");
-        overlay.className = "overlay-colorItemBox";
+        const overlay = customCreateElement("div.overlay-colorItemBox");
+
 
         colorItemButtonBox.appendChild(exChangeButton);
         colorItemButtonBox.appendChild(clipboardButton);
