b0e67dcbca3b33118fec3455d63408f2b477cabe
dnrgus1127 <dnrgus1127@naver.com>
2023-10-25 00:30:29 +0900

    FEAT

--- src/script.js
+++ src/script.js
@@ -1,27 +1,51 @@
-import { ColorPalette } from "./constructor/ColorPalette.js";
-import { ColorPaletteList } from "./constructor/ColorPaletteList.js";
-import { PaletteSelector } from "./core/PaletteSelector.js";
-import { topButtonsHandlers } from "./handler/buttons/topButtonsHandlers.js";
+import { COLOR_TYPE_HEX } from "./core/constants.js";
+import { PaletteSelector } from "./component/PaletteSelector.js";
+import { globalButtonHandlers } from "./handler/globalButtonHandlers.js";
 import { colorControllerHandlers } from "./handler/colorControllerHandlers.js";
-
-import { addPaletteHandlers } from "./handler/colorPaletteHandlers.js";
-
+import { PaletteStore } from "./store/PaletteStore.js";
 
 
-
-
-export const colorPaletteList = new ColorPaletteList();
-colorPaletteList.addColorPalette(new ColorPalette("다크모드"));
-//colorPaletteList.refreshPaletteList();
+export const paletteStore = new PaletteStore();
+paletteStore.subscribe(["currentColor", "colorType", "paletteType"], () => {
+    const { currentColor, colorType } = paletteStore.state;
+    const $colorPicker = document.getElementById("colorPicker");
+    const $searchColorInput = document.getElementById("searchColorInput");
+    $colorPicker.value = currentColor.getColorByType(COLOR_TYPE_HEX);
+    $searchColorInput.value = currentColor.getColorByType(colorType);
+})
 
 
 const $colorPalette = document.getElementById("colorPalette");
 const $colorToolBox = document.getElementById("colorToolBox");
 export const paletteSelector = new PaletteSelector($colorPalette, $colorToolBox);
-addPaletteHandlers(paletteSelector);
 colorControllerHandlers(paletteSelector);
-topButtonsHandlers(paletteSelector);
+globalButtonHandlers(paletteSelector);
 
-// 팔레트리스트 컨테이너
-//addPaletteContainerHandler();
-//contrastContainerHandler();
+
+
+paletteStore.notify("currentColor");
+paletteStore.notify("paletteType");
+
+
+
+let type = 1024;
+function reRenderItemCounts(target) {
+    if (target.innerWidth >= 1024 && type !== 1024) {
+        paletteStore.setState({ key: "itemCounts", value: 25 });
+        type = 1024;
+    }
+    if (target.innerWidth < 1024 && type !== 768) {
+        paletteStore.setState({ key: "itemCounts", value: 16 });
+        type = 768;
+    }
+    if (target.innerWidth < 768 && type !== 480) {
+        paletteStore.setState({ key: "itemCounts", value: 15 });
+        type = 480;
+    }
+}
+reRenderItemCounts(window);
+window.addEventListener("resize", (e) => {
+    reRenderItemCounts(e.target);
+})
+
+
